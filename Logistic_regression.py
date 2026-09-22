4c14e22622470b1aafd8e1ccecb5dc79e54fdeba
benscants-tech <benscants@gmail.com>
2026-09-22 19:07:20 +0100

    Implemented test of kernelised model

---
+++ Logistic_regression.py
@@ -0,0 +1,21 @@
+import numpy as np
+import pandas as pd
+
+
+n_training_examples = 400
+kernel = np.load('data/kernel.npy')
+alpha = 0.1
+batch_size = 50
+
+beta = np.zeros(n_training_examples)
+
+y_vals = np.concatenate((np.zeros(n_training_examples), (np.ones(n_training_examples))))
+def sigmoid(z):
+    return 1/(1+ np.exp(-z))
+def update_beta(indices, beta, learning_rate, y):
+    beta[indices] += learning_rate * (y[indices] - sigmoid((kernel @ beta)[indices]))
+for i in range(1000):
+    indices = np.random.choice(np.arange(n_training_examples), size=n_training_examples, replace=False)
+    update_beta(indices, beta, alpha, y_vals)
+
+np.save('data/beta.npy', beta)
---
+++ test_kernelised_model.py
@@ -0,0 +1,48 @@
+import numpy as np
+import pandas as pd
+
+
+
+steps = 201
+num_testing_examples = 100
+num_training_examples = 400
+beta = np.load('data/beta.npy')
+a = 4
+df = pd.read_csv('data/competitive_data.csv')
+competitive_testing_data = [df["y1"][-num_testing_examples * steps:], df["y2"][-num_testing_examples * steps:]]
+competitive_training_data = [df["y1"][:num_training_examples * steps], df["y2"][:num_training_examples * steps]]
+df = pd.read_csv('data/pred_prey_data.csv')
+pred_prey_testing_data = [df["y1"][-num_testing_examples * steps:], df["y2"][-num_testing_examples * steps:]]
+pred_prey_training_data = [df["y1"][:num_training_examples * steps], df["y2"][:num_training_examples * steps]]
+
+#handling testing data
+competitive_testing_data = np.array(competitive_testing_data)
+pred_prey_testing_data = np.array(pred_prey_testing_data)
+competitive_testing_data = competitive_testing_data.reshape((2, num_testing_examples, steps))
+competitive_testing_data = np.concatenate((competitive_testing_data[0, :, :], competitive_testing_data[1, :, :]), axis=1)
+pred_prey_testing_data = pred_prey_testing_data.reshape((2, num_testing_examples, steps))
+pred_prey_testing_data = np.concatenate((pred_prey_testing_data[0, :, :], pred_prey_testing_data[1, :, :]), axis=1)
+testing_data = np.concatenate((competitive_testing_data, pred_prey_testing_data), axis=0)
+
+#handling training data
+competitive_training_data = np.array(competitive_training_data)
+pred_prey_training_data = np.array(pred_prey_training_data)
+competitive_training_data = competitive_training_data.reshape((2, num_training_examples, steps))
+competitive_training_data = np.concatenate((competitive_training_data[0, :, :], competitive_training_data[1, :, :]), axis=1)
+pred_prey_training_data = pred_prey_training_data.reshape((2, num_training_examples, steps))
+pred_prey_training_data = np.concatenate((pred_prey_training_data[0, :, :], pred_prey_training_data[1, :, :]), axis=1)
+training_data = np.concatenate((competitive_training_data, pred_prey_training_data), axis=0)
+
+print(np.shape(training_data))
+print(np.shape(testing_data))
+
+y_vals = np.concatenate((np.zeros(num_testing_examples), (np.ones(num_testing_examples))))
+
+
+inner_product_matrix = training_data @ testing_data.T
+
+kernel = np.zeros(shape=np.shape(inner_product_matrix))
+for i in (np.arange(a) + 1):
+    kernel += inner_product_matrix ** i
+
+print(np.sum(y_vals == ((kernel @ beta.T) > 0.5)))
